9501b09c4a91a997f53357291ca9e334dedbb7e5
Huy Nguyen <huykst137@gmail.com>
2022-09-15 23:49:53 +0700

    add footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
-import {Container} from "./Footer.styles";
+import {
+    BrandContainer,
+    BrandLogo,
+    CompanyData,
+    CompanySupport,
+    Container,
+    Partner,
+    SupportLink,
+    Wrapper
+} from "./Footer.styles";
+import {Logo} from "../../pages/login/Login.styles";
+import companyLogo from '../../assets/images/react-movie-logo.svg'
+import cgvLogo from '../../assets/images/cgv.png'
+import lotteLogo from '../../assets/images/lotte.png'
+import bhdLogo from '../../assets/images/bhd.png'
+import galaxyLogo from '../../assets/images/galaxycine.png'
+import cinestarLogo from '../../assets/images/cinestar.png'
+import megagsLogo from '../../assets/images/megags.png'
 
 const Footer = () => {
     return (
         <Container>
-            footer
+            <Wrapper>
+                <CompanyData>
+                    <Logo src={companyLogo} />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus architecto aspernatur atque aut delectus dignissimos doloremque dolores dolorum enim eos explicabo fugiat harum id ipsa laboriosam laborum laudantium maiores nemo nesciunt nihil, nisi nobis nulla numquam possimus quaerat quia reprehenderit saepe sed sunt suscipit temporibus tenetur veritatis? Dolor, voluptates!</p>
+                </CompanyData>
+
+                <Partner>
+                    <h3>Partners</h3>
+                    <BrandContainer>
+                        <BrandLogo src={cgvLogo} />
+                        <BrandLogo src={bhdLogo} />
+                        <BrandLogo src={galaxyLogo} />
+                        <BrandLogo src={cinestarLogo} />
+                        <BrandLogo src={lotteLogo} />
+                        <BrandLogo src={megagsLogo} />
+                    </BrandContainer>
+                </Partner>
+
+                <CompanySupport>
+                    <h3>Company</h3>
+                    <SupportLink>
+                        <p>Contact us</p>
+                        <p>F.A.Q</p>
+                        <p>Careers</p>
+                        <p>Memberships</p>
+                    </SupportLink>
+                </CompanySupport>
+
+            </Wrapper>
         </Container>
     );
 };
--- src/components/Layout/Footer.styles.js
+++ src/components/Layout/Footer.styles.js
@@ -2,5 +2,82 @@ import styled from "styled-components";
 
 
 export const Container = styled.footer`
+  background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(54,54,55,1) 50%, rgba(33,33,33,1) 94%);
+  color: white;
+  padding: 2rem;
+`
 
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  
+  @media screen and (max-width: 576px) {
+    flex-wrap: wrap;
+  }
+`
+
+export const CompanyData = styled.div`
+  flex: 2;
+    
+    p {
+      margin-top: 2rem;
+      font-size: 14px;
+      color:  #B2B1B6;
+      font-weight: 500;
+    }
+`
+
+export const Partner = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem 2rem;
+  h3 {
+    font-size: 16px;
+    font-weight: bold;
+  }
+  
+`
+
+export const BrandContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin-top: 1rem;
+  gap: 5px;
+`
+
+export const BrandLogo = styled.img`
+  width: 40px;
+  height: 40px;
+  background-color: white;
+  border-radius: 50%;
+`
+
+export const CompanySupport = styled.div`
+  padding: 1rem 2rem;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h3 {
+    font-size: 16px;
+    font-weight: bold;
+  }
+`
+
+export const SupportLink = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  
+  
+  p {
+    color: #B2B1B6;
+    font-size: 14px;
+  }
 `
