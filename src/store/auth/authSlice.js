416bb40722c51606cee024413606bbf1637120fd
Huy Nguyen <huykst137@gmail.com>
2022-09-15 10:26:56 +0700

    modify authSlice by using creatAsyncThunk and handle loading

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -1,16 +1,96 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import api from "../../api/api";
+
+export const signIn = createAsyncThunk("auth/signIn", async (user) => {
+    const res = await api.request({
+        url: '/api/QuanLyNguoiDung/DangNhap',
+        method: "POST",
+        data: user
+    })
+
+    const profile = {...res.data.content}
+    delete profile.accessToken;
+
+    localStorage.setItem('token', res.data.content.accessToken)
+    return profile;
+})
+
+export const fetchProfile = createAsyncThunk("auth/fetchProfile", async () => {
+    const res = await api.request({
+        url: '/api/QuanLyNguoiDung/ThongTinTaiKhoan',
+        method: "POST",
+    })
+
+    return res.data.content;
+})
+
+export const updateProfile = createAsyncThunk("auth/updateProfile", async (updatedUser) => {
+    const res = await api.request({
+        url: "/api/QuanLyNguoiDung/CapNhatThongTinNguoiDung",
+        method: "PUT",
+        data: updatedUser,
+    })
+
+    return res.data.content;
+})
+
+const initialState = {
+    profile: null,
+    status: 'idle',
+    error: null,
+}
 
 const authSlice = createSlice({
     name: "auth",
-    initialState: {
-        profile: null,
-    },
+    initialState,
     reducers: {
         setProfile(state, action) {
             state.profile = action.payload;
-        },
+        }
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(signIn.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(signIn.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.profile = action.payload;
+            })
+            .addCase(signIn.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message;
+            })
+
+            .addCase(fetchProfile.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchProfile.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.profile = action.payload;
+            })
+            .addCase(fetchProfile.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message;
+            })
+
+            .addCase(updateProfile.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(updateProfile.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.profile = action.payload;
+            })
+            .addCase(updateProfile.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message;
+            })
     }
 })
+
+export const selectProfile = (state) => state.auth.profile;
+export const getAuthStatus = (state) => state.auth.status;
+export const getAuthError = (state) => state.auth.error;
 
 export const authActions = authSlice.actions;
 
